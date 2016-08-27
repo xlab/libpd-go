a41949de6a102f9780f45d7d7db23c985fffbb47
Maxim Kupriianov <max@kc.vc>
2016-08-28 00:53:15 +0300

    Change types.

--- libpd/helpers.go
+++ libpd/helpers.go
@@ -4,8 +4,10 @@ import "github.com/xlab/libpd-go/core"
 
 type Atom interface{}
 
-type UnknownAtom struct{}
-type NullAtom struct{}
+var (
+	UnknownAtom Atom = struct{}{}
+	NullAtom    Atom = struct{}{}
+)
 
 func convertAtomList(atoms []core.Atom) []Atom {
 	list := make([]Atom, len(atoms))
@@ -13,7 +15,7 @@ func convertAtomList(atoms []core.Atom) []Atom {
 		atoms[i].Deref()
 		switch atoms[i].AType {
 		case core.AtomNull:
-			list[i] = NullAtom{}
+			list[i] = NullAtom
 		case core.AtomFloat:
 			list[i] = atoms[i].AW.Float32()
 		case core.AtomSymbol:
@@ -21,7 +23,7 @@ func convertAtomList(atoms []core.Atom) []Atom {
 		case core.AtomPointer:
 			list[i] = atoms[i].AW.Pointer()
 		default:
-			list[i] = UnknownAtom{}
+			list[i] = UnknownAtom
 		}
 	}
 	return list
